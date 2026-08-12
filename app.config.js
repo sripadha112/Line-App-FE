a8d8f3b80b7cdfa6e5be8da8587552b06b57a5c5
sripadha112 <santhoshsripadha101@gmail.com>
2026-08-12 23:38:54 +0530

    config fix

--- app.config.js
+++ app.config.js
@@ -1,8 +1,11 @@
-module.exports = ({ config }) => {
-  const expoConfig = config || {};
+const appJson = require('./app.json');
+const withAdiRegistration = require('./plugins/withAdiRegistration');
+
+module.exports = () => {
+  const expoConfig = appJson.expo || {};
 
   return {
     ...expoConfig,
-    plugins: [...(expoConfig.plugins || []), './plugins/withAdiRegistration'],
+    plugins: [...(expoConfig.plugins || []), withAdiRegistration],
   };
 };
